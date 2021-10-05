de73017086c6685fd6720e6ed998db757731ed02
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-10-05 11:48:56 +0200

    Added new file structure, refactored project

---
+++ MandatoryExercise1/REST/Server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/courses", getCourses)
+	router.GET("/courses/:id", getCourseByID)
+	router.POST("/courses", postCourse)
+	router.DELETE("/courses/:id",deleteCourseByID)
+
+	router.Run("localhost:8080")
+}
